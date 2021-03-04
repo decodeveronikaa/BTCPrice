536b863aec084862f6358122618014ca4e4e3c3f
Veronika Markova <veronica.mkv11@gmail.com>
2021-03-04 17:49:30 -0500

    Initial commit

---
+++ BitPrice/BitPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitPriceApp.swift
+//  BitPrice
+//
+//  Created by Veronica Markova on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct BitPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
